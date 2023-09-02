6f2bf0c724953eef147e2f0c3d0a1eac80c77a7b
westhyun <pkwesst@gmail.com>
2023-09-02 13:06:45 +0900

    style: 💄 button 공통 컴포넌트 생성(PP-22)[#4]

--- src/components/@common/Button.tsx
+++ src/components/@common/Button.tsx
@@ -1,13 +1,43 @@
+import React from "react";
 import { styled } from "styled-components";
 
-interface ButtonProps {
-  text: string;
+export interface ButtonProps {
+  type: string;
+  text?: string;
+  $background: string;
+  color?: string;
+  $gridGap?: string;
+  icon?: React.ReactNode;
+  $border?: string;
+  padding?: string;
+  display?: string;
+  alignItems?: string;
+  $justifyContent?: string;
 }
 
-const Button = ({ text = "버튼", ...props }: ButtonProps) => {
-  return <ButtonWrapper {...props}>{text}</ButtonWrapper>;
+const Button = ({ type, text, icon, ...props }: ButtonProps) => {
+  return (
+    <ButtonWrapper type={type} {...props}>
+      {icon}
+      {text}
+    </ButtonWrapper>
+  );
 };
 
 export default Button;
 
-const ButtonWrapper = styled.button``;
+const ButtonWrapper = styled.button<ButtonProps>`
+  cursor: pointer;
+  width: 100%;
+  border-radius: 7px;
+  font-weight: 400;
+  font-size: 1rem;
+  background: ${(props) => props?.$background};
+  border: ${(props) => props?.$border};
+  grid-gap: ${(props) => props?.$gridGap || "62px"};
+  color: ${(props) => props?.color || "#fff"};
+  padding: ${(props) => props.padding || "12px 0px"};
+  display: ${(props) => props.display || "flex"};
+  align-items: ${(props) => props.alignItems || "center"};
+  justify-content: ${(props) => props.$justifyContent};
+`;
