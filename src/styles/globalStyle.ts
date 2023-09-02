c9e923fa53454b1afac2587dd6e68a35ce1d16d7
westhyun <pkwesst@gmail.com>
2023-09-02 13:08:32 +0900

    style: 💄 globalstyle 수정(input, fontsize reset)[#4]

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -5,6 +5,26 @@ const globalStyle = css`
     border: none;
     background-color: #888;
   }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: Noto Sans KR;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    outline: none;
+  }
 `;
 
 export default globalStyle;
