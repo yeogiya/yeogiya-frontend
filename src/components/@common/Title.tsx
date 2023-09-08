f50fc54c441af4102c70debc1df05bf687d16d92
westhyun <pkwesst@gmail.com>
2023-09-08 16:05:08 +0900

    fix: 🐛 nav bar 적용으로 인한 UI 수정(PP-90)[#6]

--- src/components/@common/Title.tsx
+++ src/components/@common/Title.tsx
@@ -2,7 +2,6 @@ import HeadingText from "./HeadingText";
 import styled from "@emotion/styled";
 
 const Title = styled(HeadingText)`
-  margin: 30px 0;
   display: flex;
   justify-content: center;
 `;
