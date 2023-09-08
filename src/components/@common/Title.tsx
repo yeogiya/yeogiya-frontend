53f5d20b828d49e3e4ddd4cd03c23717b8548e67
westhyun <pkwesst@gmail.com>
2023-09-08 15:38:46 +0900

    style: 💄 title 공통 컴포넌트 분리(PP-10)[#6]

---
+++ src/components/@common/Title.tsx
@@ -0,0 +1,10 @@
+import HeadingText from "./HeadingText";
+import styled from "@emotion/styled";
+
+const Title = styled(HeadingText)`
+  margin: 30px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export default Title;
