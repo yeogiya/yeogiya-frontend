526caa0660ce2f33ab376007639666842a2cfb7b
seohyun <pkwesst@gmail.com>
2023-09-26 09:14:46 +0900

    style: 💄 비밀번호 재설정 페이지(UI, react-hook-form적용)

--- src/pages/ResetPwPage.tsx
+++ src/pages/ResetPwPage.tsx
@@ -1,8 +1,11 @@
+import ConcealIcon from "@/assets/ConcealIcon";
 import InputUser from "@/components/@common/InputUser";
 import Layout from "@/components/@common/Layout";
 import Title from "@/components/@common/Title";
 import SubmitButton from "@/components/SubmitButton";
-import { SubmitHandler, useForm } from "react-hook-form";
+import ValidateMessage from "@/components/ValidateMessage";
+import { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 interface ResetPwProps {
   password: string;
@@ -10,9 +13,14 @@ interface ResetPwProps {
 }
 
 const ResetPwPage = () => {
+  const [passwordType, setPasswordType] = useState<string>("password");
+  const [confirmPassword, setConfirmPassword] = useState<string>("password");
   const {
+    getValues,
+    control,
+    watch,
     handleSubmit,
-    formState: { isDirty, isValid },
+    formState: { isDirty, isValid, errors },
   } = useForm<ResetPwProps>({ mode: "onChange" });
 
   const onSubmit: SubmitHandler<ResetPwProps> = (data) => {
@@ -23,12 +31,118 @@ const ResetPwPage = () => {
     <Layout>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Title as="h1">비밀번호 재설정하기</Title>
-        <InputUser labelText="변경할 비밀번호" type="text" name="password" />
-        <InputUser
-          labelText="비밀번호 확인"
-          type="text"
-          name="confirmPassword"
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required:
+              "비밀번호를 입력해주세요. (영문+숫자+특수문자 조합 8~20자)",
+            minLength: {
+              value: 8,
+              message: "영문+숫자+특수문자 조합 8~20자로 입력해주세요.",
+            },
+            validate: (value) => {
+              if (
+                !value.match(
+                  /(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+                )
+              )
+                return "영문+숫자+특수문자 조합 8~20자로 입력해주세요.";
+            },
+          }}
+          render={({ field: { onChange, value } }) => {
+            const [isPassword, setIsPassword] = useState(false);
+            return (
+              <InputUser
+                type={passwordType}
+                labelText="변경할 비밀번호"
+                onChange={(e) => {
+                  onChange(e.target.value);
+                }}
+                icon={
+                  <ConcealIcon
+                    passwordType={passwordType}
+                    setPasswordType={setPasswordType}
+                    isActive={isPassword}
+                  />
+                }
+                onFocus={() => setIsPassword(true)}
+                onBlur={() => !value && setIsPassword(false)}
+                isActive={isPassword}
+              />
+            );
+          }}
         />
+        {errors?.password?.message ? (
+          <ValidateMessage
+            color={
+              errors.password.message ===
+              "영문+숫자+특수문자 조합 8~20자로 입력해주세요."
+                ? "error"
+                : "default"
+            }
+          >
+            {errors?.password?.message}
+          </ValidateMessage>
+        ) : (
+          watch("password") && (
+            <ValidateMessage color="success">
+              사용 가능한 비밀번호입니다.
+            </ValidateMessage>
+          )
+        )}
+        <Controller
+          control={control}
+          name="confirmPassword"
+          render={({ field: { onChange, value } }) => {
+            const [isPassWordConfirm, setIsPassWordConfirm] = useState(false);
+            return (
+              <InputUser
+                type={confirmPassword}
+                labelText="비밀번호 확인"
+                onChange={(e) => onChange(e.target.value)}
+                value={value || ""}
+                icon={
+                  <ConcealIcon
+                    confirmPassword={confirmPassword}
+                    setConfirmPassword={setConfirmPassword}
+                    isActive={isPassWordConfirm}
+                  />
+                }
+                onFocus={() => setIsPassWordConfirm(true)}
+                onBlur={() => !value && setIsPassWordConfirm(false)}
+                isActive={isPassWordConfirm}
+              />
+            );
+          }}
+          rules={{
+            validate: () => {
+              const password = getValues("password");
+              const confirmPassword = getValues("confirmPassword");
+              if (!confirmPassword) return "비밀번호를 한번 더 입력해주세요.";
+              if (password !== confirmPassword)
+                return "비밀 번호가 일치하지 않습니다.";
+            },
+          }}
+        />
+        {errors?.confirmPassword?.message ? (
+          <ValidateMessage
+            color={
+              errors.confirmPassword.message ===
+              "비밀 번호가 일치하지 않습니다."
+                ? "error"
+                : "default"
+            }
+          >
+            {errors?.confirmPassword?.message}
+          </ValidateMessage>
+        ) : (
+          watch("confirmPassword") && (
+            <ValidateMessage color="success">
+              비밀 번호가 일치합니다.
+            </ValidateMessage>
+          )
+        )}
         <SubmitButton
           type="submit"
           text="비밀번호 변경하기"
