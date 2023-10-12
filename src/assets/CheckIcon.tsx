408de682c9e58013138cd82c74f8b0120e8a3fa2
seohyun <pkwesst@gmail.com>
2023-10-12 11:59:01 +0900

    style: 💄 assets 추가

---
+++ src/assets/CheckIcon.tsx
@@ -0,0 +1,22 @@
+const CheckIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="15" cy="15" r="15" fill="#614AD3" />
+      <path
+        d="M9 15.6136L12.4689 18.9478C12.5414 19.0173 12.6588 19.0175 12.7313 18.9478L21 11"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CheckIcon;
