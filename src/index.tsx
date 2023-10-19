8dc91a5e2bd781e70681ce9ff4374f457fe993a1
seohyun <pkwesst@gmail.com>
2023-10-19 17:36:43 +0900

    config: 🔧 app에 worker 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./utils/Router.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as React from "react";
+import { worker } from "./mocks/browser.ts";
+
+const queryClient = new QueryClient();
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
