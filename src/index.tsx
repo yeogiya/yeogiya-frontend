f96ac3072fa89fcea9d698b2a7d1f100946348fc
westhyun <pkwesst@gmail.com>
2023-10-23 21:33:58 +0900

    chore: 🔧 msw config[#23]

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,21 @@ import Router from "./utils/Router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import { worker } from "./mocks/browser.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 if (process.env.NODE_ENV === "development") {
-  worker.start();
+  worker.start({
+    onUnhandledRequest: "bypass",
+  });
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <Router />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <Router />
+    <ReactQueryDevtools initialIsOpen={false} />
+    {/* </React.StrictMode> */}
   </QueryClientProvider>
 );
