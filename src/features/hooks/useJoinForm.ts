0cd14fcd576684e13ceb52be8760037dcf0678b6
seohyun <pkwesst@gmail.com>
2023-10-30 17:44:32 +0900

    feat: ⚡️ react-hook-form custom hook 추가[#23]

--- src/features/hooks/useJoinForm.ts
+++ src/features/hooks/useJoinForm.ts
@@ -1,6 +1,4 @@
 import { JoinProps } from "@/pages/JoinPage";
-import axios from "axios";
-import { useState } from "react";
 import { Control, useController } from "react-hook-form";
 
 const useJoinForm = (control: Control<JoinProps>) => {
@@ -16,74 +14,78 @@ const useJoinForm = (control: Control<JoinProps>) => {
     },
   });
 
-  // const { field: confirmEmail, fieldState: confirmEmailState } = useController({
-  //   name: "confirmEmail",
-  //   control,
-  //   rules: {
-  //     required: "중복 체크 해주세요",
-  //     validate: {
-  //       checkConfirmEmail: (value) => {
-  //         if (!value) return "노노";
-  //       },
-  //     },
-  //   },
-  // });
+  const { field: confirmEmail, fieldState: confirmEmailState } = useController({
+    name: "confirmEmail",
+    control,
+    rules: {
+      required: "이메일 중복 확인을 해주세요.",
+    },
+  });
 
-  // const { field: id, fieldState: idState } = useController({
-  //   name: "id",
-  //   control,
-  //   rules: {
-  //     required: "아이디를 입력해주세요.",
-  //   },
-  // });
+  const { field: id, fieldState: idState } = useController({
+    name: "id",
+    control,
+    rules: {
+      required: "아이디를 입력해주세요.",
+    },
+  });
 
-  // const { field: nickname, fieldState: nicknameState } = useController({
-  //   name: "nickname",
-  //   control,
-  //   rules: {
-  //     required: "닉네임을 입력해주세요.",
-  //   },
-  // });
+  const { field: confirmId, fieldState: confirmIdState } = useController({
+    name: "confirmId",
+    control,
+    rules: {
+      required: "아이디 중복 확인을 해주세요.",
+    },
+  });
 
-  // const { field: password, fieldState: passwordState } = useController({
-  //   name: "password",
-  //   control,
-  //   rules: {
-  //     required: "비밀번호를 입력해주세요. (영문+숫자+특수문자 조합 8~20자)",
-  //     validate: (value) => {
-  //       if (!value.match(/(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/))
-  //         return "영문+숫자+특수문자 조합 8~20자로 입력해주세요.";
-  //     },
-  //   },
-  // });
-  // const { field: confirmPassword, fieldState: confirmPasswordState } =
-  //   useController({
-  //     name: "confirmPassword",
-  //     control,
-  //     rules: {
-  //       required: "비밀번호를 한번 더 입력해주세요.",
-  //       validate: (value) => {
-  //         value === password.value ? true : "비밀번호가 일치하지 않습니다.";
-  //         if (!confirmPassword) return "비밀번호를 한번 더 입력해주세요.";
-  //       },
-  //     },
-  //   });
+  const { field: nickname, fieldState: nicknameState } = useController({
+    name: "nickname",
+    control,
+    rules: {
+      required: "닉네임을 입력해주세요.",
+    },
+  });
+
+  const { field: password, fieldState: passwordState } = useController({
+    name: "password",
+    control,
+    rules: {
+      required: "비밀번호를 입력해주세요. (영문+숫자+특수문자 조합 8~20자)",
+      validate: (value) => {
+        if (!value.match(/(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/))
+          return "영문+숫자+특수문자 조합 8~20자로 입력해주세요.";
+      },
+    },
+  });
+  const { field: confirmPassword, fieldState: confirmPasswordState } =
+    useController({
+      name: "confirmPassword",
+      control,
+      rules: {
+        required: "비밀번호를 한번 더 입력해주세요.",
+        validate: (value) => {
+          return value === password.value
+            ? true
+            : "비밀번호가 일치하지 않습니다.";
+        },
+      },
+    });
 
   return {
     email,
     emailState,
-    // confirmEmail,
-    // confirmEmailState,
-    // id,
-    // idState,
-    // confirmId,
-    // confirmIdState,
-    // nickname,
-    // nicknameState,
-    // password,
-    // passwordState,
-    // confirmPassword,
-    // confirmPasswordState,
+    confirmEmail,
+    confirmEmailState,
+    id,
+    idState,
+    confirmId,
+    confirmIdState,
+    nickname,
+    nicknameState,
+    password,
+    passwordState,
+    confirmPassword,
+    confirmPasswordState,
   };
 };
 
