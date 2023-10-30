ee247478bed72d8f104c9ad96df5633e60ac123a
seohyun <pkwesst@gmail.com>
2023-10-30 17:40:37 +0900

    chore: 🩹 api base url export[#23]

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,7 +1,7 @@
 import { JoinProps } from "./../pages/JoinPage";
 import { useQuery } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
-const BASE_URL = "http://13.209.150.130:8080/api/public/v1.0.0";
+import axios from "axios";
+export const BASE_URL = "http://13.209.150.130:8080/api/public/v1.0.0";
 
 // export const checkEmailApi = (uncheckedEmail: string | null) => {
 //   const { error, isLoading, data, refetch } = useQuery({
