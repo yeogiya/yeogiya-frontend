2775339ba70b577d9b12912375aebc30edf018bb
westhyun <pkwesst@gmail.com>
2023-11-05 23:58:52 +0900

    feat: ✨ user api 추가[#23]

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,25 +1,54 @@
-import { JoinProps } from "./../pages/JoinPage";
-import { useQuery } from "@tanstack/react-query";
+import { JoinProps } from "@/pages/join/JoinPage";
 import axios from "axios";
+
 export const BASE_URL = "http://13.209.150.130:8080/api/public/v1.0.0";
 
-// export const checkEmailApi = (uncheckedEmail: string | null) => {
-//   const { error, isLoading, data, refetch } = useQuery({
-//     queryKey: ["join", "email", uncheckedEmail],
-//     queryFn: async () => {
-//       const res = axios.get("/mock/check-email", {
-//         params: {
-//           email: uncheckedEmail,
-//         },
-//       });
-//       return res;
-//     },
-//     enabled: !!uncheckedEmail,
-//   });
-//   return data;
-// };
+export const joinApi = async (
+  params: Partial<JoinProps> & { loginType: string }
+) => {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/members/sign-up`, params);
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-export const checkEmailDuplicate = (email: string) =>
-  axios.get(`/mock/check-email?email=${email}`);
+export const checkEmailApi = async (email: string) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/members/email-exists`, {
+      params: {
+        email,
+      },
+    });
+    return data.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-export const checkEmailApi = (email: string) => {};
+export const checkIdApi = async (id: string) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/members/id-exists`, {
+      params: {
+        id,
+      },
+    });
+    return data.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const checkNicknameApi = async (nickname: string) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/members/nickname-exists`, {
+      params: {
+        nickname,
+      },
+    });
+    return data.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
