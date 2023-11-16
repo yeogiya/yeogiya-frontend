05b50688d79ea5a72e065e96cd292ac597b7d39a
westhyun <pkwesst@gmail.com>
2023-11-17 00:40:46 +0900

    feat: ⚡️ 평점 기능 추가[#45]

--- src/pages/diary/create/components/Rating.tsx
+++ src/pages/diary/create/components/Rating.tsx
@@ -1,16 +1,21 @@
 import StarIcon from "@/assets/images/StarIcon";
-import theme from "@/styles/theme";
 import styled from "@emotion/styled";
 import { useState } from "react";
 
 const Rating = () => {
+  const [rating, setRating] = useState<number>(0);
+
+  const handleStarClick = (value: number) => {
+    setRating(value);
+  };
+
   return (
     <RatingStyle>
-      <StarIcon />
-      <StarIcon />
-      <StarIcon />
-      <StarIcon />
-      <StarIcon />
+      <StarIcon value={1} onClick={() => handleStarClick(1)} rating={rating} />
+      <StarIcon value={2} onClick={() => handleStarClick(2)} rating={rating} />
+      <StarIcon value={3} onClick={() => handleStarClick(3)} rating={rating} />
+      <StarIcon value={4} onClick={() => handleStarClick(4)} rating={rating} />
+      <StarIcon value={5} onClick={() => handleStarClick(5)} rating={rating} />
     </RatingStyle>
   );
 };
