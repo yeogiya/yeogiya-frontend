59d51418f63d3106d9fc927c4740d59c4f2e964d
westhyun <pkwesst@gmail.com>
2023-11-19 20:57:00 +0900

    feat: ⚡️ 공간일기쓰기 평점 기능 추가[#45]

--- src/pages/diary/create/components/Rating.tsx
+++ src/pages/diary/create/components/Rating.tsx
@@ -1,29 +1,58 @@
 import StarIcon from "@/assets/images/StarIcon";
+import theme from "@/styles/theme";
 import styled from "@emotion/styled";
 import { useState } from "react";
 
 const Rating = () => {
-  const [rating, setRating] = useState<number>(0);
+  const [clicked, setClicked] = useState([false, false, false, false, false]);
 
-  const handleStarClick = (value: number) => {
-    setRating(value);
+  const handleClickStar = (index: number) => {
+    let clickedStar = [...clicked];
+    for (let i = 0; i < 5; i++) {
+      clickedStar[i] = i <= index ? true : false;
+    }
+    setClicked(clickedStar);
   };
 
+  const STARS = [0, 1, 2, 3, 4];
+
   return (
-    <RatingStyle>
-      <StarIcon value={1} onClick={() => handleStarClick(1)} rating={rating} />
-      <StarIcon value={2} onClick={() => handleStarClick(2)} rating={rating} />
-      <StarIcon value={3} onClick={() => handleStarClick(3)} rating={rating} />
-      <StarIcon value={4} onClick={() => handleStarClick(4)} rating={rating} />
-      <StarIcon value={5} onClick={() => handleStarClick(5)} rating={rating} />
-    </RatingStyle>
+    <Stars>
+      {STARS.map((star, idx) => {
+        return (
+          <StarIcon
+            key={idx}
+            onClick={() => handleClickStar(star)}
+            className={clicked[star] && "fillStar"}
+          />
+        );
+      })}
+    </Stars>
   );
 };
 
-const RatingStyle = styled.div`
-  display: flex;
-  gap: 4px;
+const Stars = styled.div`
+  display: inline-block;
+  max-width: fit-content;
   margin-bottom: 16px;
+
+  svg {
+    fill: ${theme.color.black30};
+    cursor: pointer;
+    padding-right: 4px;
+  }
+
+  :hover svg {
+    fill: ${theme.color.purple};
+  }
+
+  svg:hover ~ svg {
+    fill: ${theme.color.black30};
+  }
+
+  .fillStar {
+    fill: ${theme.color.purple};
+  }
 `;
 
 export default Rating;
