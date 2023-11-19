df18151696738521f954da2bc112707e9b4d2789
westhyun <pkwesst@gmail.com>
2023-11-19 20:57:12 +0900

    feat: ⚡️ 공간일기쓰기 파일 업로드 기능 추가[#45]

--- src/pages/diary/create/components/UploadImage.tsx
+++ src/pages/diary/create/components/UploadImage.tsx
@@ -1,24 +1,56 @@
 import { PlusIcon } from "@/assets";
 import theme from "@/styles/theme";
 import styled from "@emotion/styled";
+import { useRef, useState } from "react";
 
-const UploadImage = ({ inputRef, onUploadImage }) => {
+const UploadImage = () => {
+  const fileInput = useRef(null);
+  const [showImages, setShowImages] = useState([]);
+
+  const handleButtonClick = (e) => {
+    fileInput.current.click();
+  };
+
+  const saveImgFile = (event) => {
+    const imageLists = event.target.files;
+    let imageUrlLists = [...showImages];
+
+    for (let i = 0; i < imageLists.length; i++) {
+      const currentImageUrl = URL.createObjectURL(imageLists[i]);
+      imageUrlLists.push(currentImageUrl);
+    }
+
+    if (imageUrlLists.length > 10) {
+      imageUrlLists = imageUrlLists.slice(0, 10);
+    }
+
+    setShowImages(imageUrlLists);
+  };
   return (
-    <UploadImageStyle>
-      <PlusIcon fill={`${theme.color.black35}`} />
-      <Input
-        type="file"
-        accept="image/*"
-        // ref={inputRef}
-        // onChange={onUploadImage}
-      />
-    </UploadImageStyle>
+    <>
+      <UploadImageStyle onClick={handleButtonClick}>
+        <PlusIcon fill={`${theme.color.black35}`} />
+        <Input
+          ref={fileInput}
+          multiple
+          type="file"
+          accept="image/*"
+          onChange={saveImgFile}
+        />
+      </UploadImageStyle>
+      {showImages?.map((image, id) => (
+        <UploadImageStyle>
+          <img src={image} key={id} alt={`${image}-${id}`} />
+        </UploadImageStyle>
+      ))}
+    </>
   );
 };
 export default UploadImage;
 
 const UploadImageStyle = styled.div`
   display: flex;
+  flex-direction: row;
   width: 100%;
   max-width: 89px;
   height: 100%;
@@ -27,11 +59,22 @@ const UploadImageStyle = styled.div`
   border: 1px dashed ${theme.color.black35};
   align-items: center;
   justify-content: center;
+  cursor: pointer;
 
   svg {
     display: flex;
     max-width: 27px;
     max-height: 27px;
+  }
+
+  img {
+    display: flex;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    max-width: 89px;
+    min-height: 89px;
+    margin-right: 10px;
   }
 `;
 
