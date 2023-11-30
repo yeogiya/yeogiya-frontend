74153af11de1508b8d0e8286828f721c75e80cc7
seohyun <pkwesst@gmail.com>
2023-11-30 17:21:11 +0900

    style: 💄 변경된 icon으로 업데이트[#62]

--- src/assets/images/CheckIcon.tsx
+++ src/assets/images/CheckIcon.tsx
@@ -1,24 +1,51 @@
 import { SVGProps } from "@/types/assets";
-import theme from "@/styles/theme";
 
 const CheckIcon = ({ css }: SVGProps) => {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       width="30"
       height="30"
       viewBox="0 0 30 30"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       css={css}
     >
-      <circle cx="15" cy="15" r="15" fill={theme.color.purple} />
+      <circle cx="15" cy="15" r="15" fill="url(#paint0_linear_852_1873)" />
+      <path
+        d="M9 15.6136L12.4689 18.9478C12.5414 19.0173 12.6588 19.0175 12.7313 18.9478L21 11"
+        fill="url(#paint1_linear_852_1873)"
+      />
       <path
         d="M9 15.6136L12.4689 18.9478C12.5414 19.0173 12.6588 19.0175 12.7313 18.9478L21 11"
         stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
+      <defs>
+        <linearGradient
+          id="paint0_linear_852_1873"
+          x1="0"
+          y1="15"
+          x2="30"
+          y2="15"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#C56CD6" />
+          <stop offset="1" stop-color="#3425AF" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_852_1873"
+          x1="9"
+          y1="15"
+          x2="21"
+          y2="15"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#C56CD6" />
+          <stop offset="1" stop-color="#3425AF" />
+        </linearGradient>
+      </defs>
     </svg>
   );
 };
--- src/assets/images/PlusIcon.tsx
+++ src/assets/images/PlusIcon.tsx
@@ -1,5 +1,5 @@
-import { SVGProps } from "@/types/assets";
 import theme from "@/styles/theme";
+import { SVGProps } from "@/types/assets";
 
 interface PlusIconProps extends SVGProps {
   fill?: string;
@@ -16,11 +16,24 @@ const PlusIcon = ({ css, fill }: PlusIconProps) => {
       css={css}
     >
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.2 1.2C16.2 0.537259 15.6627 0 15 0C14.3372 0 13.8 0.537259 13.8 1.2V13.8012L1.2 13.8012C0.537259 13.8012 0 14.3384 0 15.0012C0 15.6639 0.537259 16.2012 1.2 16.2012H13.8V28.8C13.8 29.4627 14.3372 30 15 30C15.6627 30 16.2 29.4627 16.2 28.8V16.2012H28.8C29.4627 16.2012 30 15.6639 30 15.0012C30 14.3384 29.4627 13.8012 28.8 13.8012L16.2 13.8012V1.2Z"
-        fill={fill || `${theme.color.purple}`}
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M16.2 1.2C16.2 0.537259 15.6627 0 15 0C14.3372 0 13.8 0.537259 13.8 1.2V13.8012L1.2 13.8012C0.537258 13.8012 0 14.3384 0 15.0012C0 15.6639 0.537258 16.2012 1.2 16.2012H13.8V28.8C13.8 29.4627 14.3372 30 15 30C15.6627 30 16.2 29.4627 16.2 28.8V16.2012H28.8C29.4627 16.2012 30 15.6639 30 15.0012C30 14.3384 29.4627 13.8012 28.8 13.8012L16.2 13.8012V1.2Z"
+        fill="url(#paint0_linear_852_1966)"
       />
+      <defs>
+        <linearGradient
+          id="paint0_linear_852_1966"
+          x1="0"
+          y1="15"
+          x2="30"
+          y2="15"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color={fill || `${theme.color.purple10}`} />
+          <stop offset="1" stop-color={fill || `${theme.color.navy}`} />
+        </linearGradient>
+      </defs>
     </svg>
   );
 };
