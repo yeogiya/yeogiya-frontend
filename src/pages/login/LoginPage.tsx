c57683674d63e33d95399ab159ac49d1edba9254
westhyun <pkwesst@gmail.com>
2023-12-04 23:09:46 +0900

    refac: ♻️ 로그인 페이지 리팩토링[#24]

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,103 +1,80 @@
 import Button, { ButtonProps } from "@/components/@common/Button";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form";
 import { GoogleLogo, KakaoLogo } from "@/assets";
-
 import IconButton from "@/components/IconButton";
-import InputUser from "@/components/@common/InputUser";
 import Layout from "@/components/@common/Layout";
 import LinkText from "@/components/@common/LinkText";
 import { PATH } from "@/utils/routes";
 import Title from "@/components/@common/Title";
 import styled from "@emotion/styled";
 import theme from "@/styles/theme";
+import { DevTool } from "@hookform/devtools";
+import InputId from "../join/components/InputId";
+import InputPassword from "../join/components/InputPassword";
+import useLoginForm from "@/features/hooks/useLoginForm";
+import { loginApi } from "@/apis/user";
+import { useNavigate } from "react-router-dom";
 
-interface LoginProps {
+export interface LoginProps {
   id: string;
   password: string;
 }
 
 const LoginPage = () => {
-  const {
-    handleSubmit,
-    formState: { errors },
-    control,
-  } = useForm<LoginProps>({
+  const navigate = useNavigate();
+  const { handleSubmit, control } = useForm<LoginProps>({
+    mode: "onBlur",
     defaultValues: {
       id: "",
       password: "",
     },
   });
 
-  const onSubmit: SubmitHandler<LoginProps> = (data) => {
+  const { id, idState, password, passwordState } = useLoginForm(control);
+
+  const onSubmit: SubmitHandler<LoginProps> = async (data) => {
     const { id, password } = { ...data };
-    console.log(id, password);
+
+    await loginApi({ id, password });
+
+    navigate("/");
   };
 
   return (
     <Layout maxWidth="328px">
       <Title as="h1">로그인</Title>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="id"
-          render={({ field: { onChange, value } }) => {
-            return (
-              <InputUser
-                type="text"
-                labelText="아이디"
-                placeholder="아이디를 입력해주세요"
-                onChange={(e) => onChange(e.target.value)}
-                value={value || ""}
-              />
-            );
-          }}
-        />
-        {errors.id && <span>This field is required</span>}
-
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { onChange, value } }) => {
-            return (
-              <InputUser
-                type="password"
-                labelText="비밀번호"
-                placeholder="비밀번호를 입력해주세요"
-                onChange={(e) => onChange(e.target.value)}
-                value={value || ""}
-              />
-            );
-          }}
-        />
+        <InputId id={id} idState={idState} />
+        <InputPassword password={password} passwordState={passwordState} />
         <LoginButton
           type="submit"
           text="로그인"
           background={theme.color.purple}
           justifyContent="center"
         />
+        <ButtonContainer>
+          <LinkText to={PATH.JOIN} text="회원가입" top={0} />
+          <ButtonWrapper>
+            <LinkText to={PATH.FIND_ID} text="아이디 찾기" top={0} />
+            <span>/</span>
+            <LinkText to={PATH.FIND_PW} text="비밀번호 찾기" top={0} />
+          </ButtonWrapper>
+        </ButtonContainer>
+        <IconButton
+          type="submit"
+          text="Google로 로그인"
+          background={theme.color.white}
+          border={`1px solid ${theme.color.black35}`}
+          icon={<GoogleLogo />}
+        />
+        <IconButton
+          type="submit"
+          text="카카오로 로그인"
+          background={theme.color.yellow}
+          icon={<KakaoLogo />}
+        />
       </form>
-
-      <ButtonContainer>
-        <LinkText to={PATH.JOIN} text="회원가입" marginTop={0} />
-        <ButtonWrapper>
-          <LinkText to={PATH.FIND_ID} text="아이디 찾기" marginTop={0} />
-          <span>/</span>
-          <LinkText to={PATH.FIND_PW} text="비밀번호 찾기" marginTop={0} />
-        </ButtonWrapper>
-      </ButtonContainer>
-      <IconButton
-        type="submit"
-        text="Google로 로그인"
-        background={theme.color.white}
-        border={`1px solid ${theme.color.black35}`}
-        icon={<GoogleLogo />}
-      />
-      <IconButton
-        type="submit"
-        text="카카오로 로그인"
-        background={theme.color.yellow}
-        icon={<KakaoLogo />}
-      />
+      <DevTool control={control} />
     </Layout>
   );
 };
