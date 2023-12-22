03e79b16e79a11389f3566b1ce4be8e85c19dd88
westhyun <pkwesst@gmail.com>
2023-12-23 02:00:30 +0900

    refac: ♻️ constants에 menu.ts 이동[#97]

--- src/constants/menus.ts
+++ src/constants/menus.ts
@@ -1,20 +1,20 @@
-import { PATH } from "./routes";
+import { PATH } from "../utils/routes";
 
 export type MenuItemType = "diary" | "member" | "login";
 
-export interface MenuItem {
+export interface MenuItemProps {
   type: MenuItemType;
   title: string;
   path: string;
 }
 
-export const MENU_ITEM: MenuItem[] = [
+export const MENU_ITEM: MenuItemProps[] = [
   { type: "diary", title: "공간일기 쓰기", path: PATH.DIARY_LIST },
   { type: "member", title: "회원가입", path: PATH.JOIN },
   { type: "member", title: "로그인", path: PATH.LOGIN },
 ];
 
-export const USER_MENU_ITEM = (nickname: string): MenuItem[] => [
+export const USER_MENU_ITEM = (nickname: string): MenuItemProps[] => [
   { type: "diary", title: "공간일기 쓰기", path: PATH.DIARY_LIST },
   { type: "login", title: `${nickname} 님`, path: PATH.MY },
 ];
