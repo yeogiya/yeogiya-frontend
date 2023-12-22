530b91d0d5bec0f8fc43f84babe8af3b6fc56934
westhyun <pkwesst@gmail.com>
2023-12-23 02:00:13 +0900

    refac: ♻️ constants에 queryKey.ts 이동[#97]

--- src/apis/queryKey.ts
+++
@@ -1,4 +0,0 @@
-export const users = {
-  info: ["users"],
-  checkEmail: (email: string) => ["email", email],
-};
---
+++ src/constants/queryKey.ts
@@ -0,0 +1,4 @@
+export const users = {
+  info: ["users"],
+  checkEmail: ["email"],
+};
