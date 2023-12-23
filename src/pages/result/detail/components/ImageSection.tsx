668abdd0370f9168e2b6d7a1799dec78e2c2048b
parkgadan <parkgadani@gmail.com>
2023-12-23 20:52:39 +0900

    style: 💄 ImageSection[#76]

---
+++ src/pages/result/detail/components/ImageSection.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+import Carousel from "./Carousel";
+import Image from "@/components/@common/Image";
+import styled from "@emotion/styled";
+import theme from "@/styles/theme";
+
+const ImageSection = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    // TODO getImageAPI
+    const getImage = [
+      "https://dummyimage.com/288x288/614ad3/ffffff",
+      "https://dummyimage.com/288x288/fffff/000000",
+      "https://dummyimage.com/288x288/614ad3/ffffff",
+      "https://dummyimage.com/288x288/fffff/000000",
+      "https://dummyimage.com/288x288/614ad3/ffffff",
+      "https://dummyimage.com/288x288/00000/fffff",
+      "https://dummyimage.com/288x288/614ad3/ffffff",
+    ];
+    setImages(getImage);
+  }, []);
+
+  return (
+    <Layout>
+      <ImageWrapper>
+        {images.length > 0 ? (
+          <Carousel images={images} />
+        ) : (
+          <DefaultImage>등록한 사진이 없습니다.</DefaultImage>
+        )}
+      </ImageWrapper>
+    </Layout>
+  );
+};
+
+const Layout = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 18rem;
+  display: flex;
+  justify-content: center;
+`;
+
+const DefaultImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  color: ${theme.color.white};
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default ImageSection;
