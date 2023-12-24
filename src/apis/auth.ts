2139335b8cae82b171c442893f18ac4b15051418
westhyun <pkwesst@gmail.com>
2023-12-24 18:40:27 +0900

    feat: ⚡️ token reissue 기능 추가[#103]

---
+++ src/apis/auth.ts
@@ -0,0 +1,29 @@
+import { TOKEN } from "@/constants/token";
+import { URL } from "@/constants/url";
+
+let reissuePromise: Promise<Response> | null = null;
+
+export const reissueToken = async () => {
+  if (reissuePromise !== null) {
+    return reissuePromise;
+  }
+
+  reissuePromise = fetch(URL.PUBLIC_BASE_URL + "/tokens/refresh", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization-Refresh": `Bearer ${localStorage.getItem(
+        TOKEN.REFRESH_TOKEN
+      )}`,
+    },
+  });
+
+  const response = await reissuePromise;
+  reissuePromise = null;
+
+  if (!response.ok) {
+    throw new Error("네트워크 통신 중 에러가 발생했습니다.");
+  }
+
+  return response;
+};
