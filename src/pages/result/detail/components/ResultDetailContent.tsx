cef32c7ed1bcec3ef31804e2f9210f88abd235c6
parkgadan <parkgadani@gmail.com>
2023-12-25 12:17:10 +0900

    style: 💄 식당 더보기 상세 페이지 - placeInfo[#76]

--- src/pages/result/detail/components/ResultDetailContent.tsx
+++ src/pages/result/detail/components/ResultDetailContent.tsx
@@ -4,10 +4,57 @@ import theme from "@/styles/theme";
 import styled from "@emotion/styled";
 import { useEffect } from "react";
 
-const ResultDetailContent = () => {
+interface ResultDetailContentProps {
+  address?: string;
+  localAddress?: string;
+  businessHours?: string;
+  phoneNumber?: string;
+  parking?: string;
+  menu?: string[];
+}
+
+const ResultDetailContent = ({
+  address = "서울특별시 강남구 역삼동 78길 2",
+  localAddress = "서울특별시 강남구 역삼동 123-12",
+  businessHours = "매일 00:00 ~ 00:00",
+  phoneNumber = "02 - 123 - 1234",
+  parking = "주차, 발렛",
+  menu = ["돼지 김치찌개", "참치 김치찌개"],
+}: ResultDetailContentProps) => {
   return (
     <Container>
-      <PlaceInfo></PlaceInfo>
+      <PlaceInfo>
+        <TextLayout>
+          <SubTitle>위치</SubTitle>
+          <TextLayout column address>
+            <Text>{address}</Text>
+            <TextLayout address>
+              <span>지번</span>
+              {localAddress}
+            </TextLayout>
+          </TextLayout>
+        </TextLayout>
+        <TextLayout>
+          <SubTitle>영업시간</SubTitle>
+          <Text>{businessHours}</Text>
+        </TextLayout>
+        <TextLayout>
+          <SubTitle>전화번호</SubTitle>
+          <Text>{phoneNumber}</Text>
+        </TextLayout>
+        <TextLayout>
+          <SubTitle>주차</SubTitle>
+          <Text>{parking}</Text>
+        </TextLayout>
+        <TextLayout>
+          <SubTitle>메뉴</SubTitle>
+          <TextLayout column>
+            {menu.map((text) => {
+              return <Text key={text}>{text}</Text>;
+            })}
+          </TextLayout>
+        </TextLayout>
+      </PlaceInfo>
       <MapWrapper>
         <Map width="26.125rem" height="27rem" />
         <MapButton>길 찾기</MapButton>
@@ -28,6 +75,39 @@ const PlaceInfo = styled.div`
   height: 25.9375rem;
   border-radius: 0.875rem;
   border: 1px solid ${theme.color.black35};
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.875rem;
+  padding: 1.25rem;
+`;
+
+const TextLayout = styled.div<{ column?: boolean; address?: boolean }>`
+  display: flex;
+  flex-direction: ${({ column }) => column && "column"};
+  row-gap: ${({ column, address }) =>
+    (address && "0.75rem") || (column && "1.25rem")};
+  column-gap: ${({ address }) => address && "0.5rem"};
+  color: ${({ address }) => address && theme.color.black50};
+  font-size: ${({ address }) => address && "0.875rem"};
+
+  span {
+    padding: 0.125rem 0.5625rem;
+    border-radius: 0.3125rem;
+    border: 1px solid ${theme.color.black20};
+  }
+`;
+
+const SubTitle = styled.p`
+  color: ${theme.color.black89};
+  font-size: 1rem;
+  font-weight: 400;
+  width: 8.125rem;
+`;
+
+const Text = styled.p`
+  color: ${theme.color.black89};
+  font-size: 1rem;
+  font-weight: 400;
 `;
 
 const MapWrapper = styled.div`
