e537f91faf97ff373cb5a252b8d0308aa425b973
seohyun <pkwesst@gmail.com>
2023-12-27 11:02:55 +0900

    refac: ♻️ 이메일 중복 체크 refactoring[#121]

--- src/features/hooks/useJoinForm.ts
+++ src/features/hooks/useJoinForm.ts
@@ -1,12 +1,8 @@
 import { Control, useController } from "react-hook-form";
 import { getCheckEmail, getCheckId, getCheckNickname } from "@/apis/user";
 import { JoinProps } from "@/pages/join/JoinPage";
+import { CheckDuplicationProps } from "@/types/users";
 
-export interface getCheckEmailProps {
-  body: {
-    duplicated: boolean;
-  };
-}
 const useJoinForm = (control: Control<JoinProps>) => {
   const { field: email, fieldState: emailState } = useController({
     name: "email",
@@ -18,10 +14,9 @@ const useJoinForm = (control: Control<JoinProps>) => {
         message: "이메일 형식이 잘못 되었습니다.",
       },
       validate: async (value) => {
-        const { duplicated } = await getCheckEmail(value).then(
-          ({ data }) => data.body
-        );
-        if (duplicated) return "이미 가입된 이메일입니다.";
+        const response = await getCheckEmail(value);
+        const { body } = response as CheckDuplicationProps;
+        if (body.duplicated) return "이미 가입된 이메일입니다.";
       },
     },
   });
@@ -44,10 +39,10 @@ const useJoinForm = (control: Control<JoinProps>) => {
         message: "영어 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다.",
       },
       validate: async (value) => {
-        const { duplicated } = await getCheckId(value).then(
-          ({ data }) => data.body
-        );
-        if (duplicated) return "이미 사용 중인 아이디입니다.";
+        const response = await getCheckId(value);
+        const { body } = response as CheckDuplicationProps;
+
+        if (body.duplicated) return "이미 사용 중인 아이디입니다.";
       },
     },
   });
@@ -58,10 +53,9 @@ const useJoinForm = (control: Control<JoinProps>) => {
     rules: {
       required: "닉네임을 입력해주세요.",
       validate: async (value) => {
-        const { duplicated } = await getCheckNickname(value).then(
-          ({ data }) => data.body
-        );
-        if (duplicated) return "이미 사용 중인 닉네임입니다.";
+        const response = await getCheckNickname(value);
+        const { body } = response as CheckDuplicationProps;
+        if (body.duplicated) return "이미 사용 중인 닉네임입니다.";
       },
     },
   });
