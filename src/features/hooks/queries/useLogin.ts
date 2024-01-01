e3fbbf96b928382b8cb95a955ff6bc674c750aa9
westhyun <pkwesst@gmail.com>
2024-01-01 15:57:26 +0900

    refac: ♻️ redux persist 제거 & useInfo hook 적용[#138]

--- src/features/hooks/queries/useLogin.ts
+++ src/features/hooks/queries/useLogin.ts
@@ -1,26 +1,16 @@
 import { JoinProps } from "@/pages/join/JoinPage";
-import {
-  UseMutationOptions,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { useToken } from "../useToken";
 import { postLogin } from "@/apis/user";
 import { AxiosResponseHeaders } from "axios";
 import { PATH } from "@/utils/routes";
-import { useUserInfo } from "./useUserInfo";
-import { useAppDispatch } from "../useAppDispatch";
-import { createUser } from "@/store/userSlice";
 
 export const useLogin = (
   options?: UseMutationOptions<unknown, unknown, Partial<JoinProps>, unknown>
 ) => {
-  const queryClient = useQueryClient();
-  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { updateToken } = useToken();
-  const { data: userInfo } = useUserInfo();
 
   return useMutation({
     mutationFn: postLogin,
@@ -30,16 +20,6 @@ export const useLogin = (
 
       updateToken(accessToken || "", refreshToken || "");
 
-      dispatch(
-        createUser({
-          email: userInfo.body.email,
-          id: userInfo.body.id,
-          nickname: userInfo.body.nickname,
-          profileImg: userInfo.body.profileImageUrl,
-        })
-      );
-
-      queryClient.invalidateQueries();
       navigate(PATH.HOME);
     },
     ...options,
