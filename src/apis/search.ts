d9cf51f4c03dcb60a1e5c80bc8df3b05b65a560d
parkgadan <parkgadani@gmail.com>
2024-01-08 21:36:43 +0900

    feat: ✨ 위치 검색 API[#67]

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -10,3 +10,15 @@ export const getSearchDetail = (placeId: string, keyword: string) => {
     `${URL.SEARCH_DETAIL}?placeId=${placeId}&keyword=${keyword}`
   );
 };
+
+export const getLocationSearch = (
+  keyword: string,
+  lat: number,
+  lng: number,
+  page: number,
+  size: number
+) => {
+  return httpClient.get(
+    `${URL.LOCATION_SEARCH}?keyword=${keyword}&lat=${lat}&lng=${lng}&page=${page}&size=${size}`
+  );
+};
